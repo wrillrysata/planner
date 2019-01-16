f8b7364a92433a1106e465731202a98b126187be
Ijebusoma <vanessaosuka@gmail.com>
2019-01-16 10:02:06 +0100

    Add favicon

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const favicon = require('express-favicon');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
